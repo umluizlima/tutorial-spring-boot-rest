1bde81c072b83d334d52e3a2ca3dcd6b954ccc3a
umluizlima <umluizlima@gmail.com>
2019-03-07 10:28:44 -0300

    Add Employee class

---
+++ src/main/java/payroll/Employee.java
@@ -0,0 +1,21 @@
+package payroll;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
