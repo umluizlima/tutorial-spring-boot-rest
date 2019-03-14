9a9429614848ffa966e0920ce60dd07136879498
umluizlima <umluizlima@gmail.com>
2019-03-14 11:27:34 -0300

    Change request mapping

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -5,22 +5,24 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    @GetMapping("/employees")
+    @GetMapping("")
     List<Employee> all() {
         return repository.findAll();
     }
 
-    @PostMapping("/employees")
+    @PostMapping("")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
