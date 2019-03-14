b8fa6e06ff24a9ee9a1ff8d8cbe54116390465e1
umluizlima <umluizlima@gmail.com>
2019-03-14 11:31:43 -0300

    Add get one employee endpoint

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -3,6 +3,7 @@ package payroll;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,11 @@ public class EmployeeController {
     @PostMapping("")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
+    }
+
+    @GetMapping("/{id}")
+    Employee one(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 
 }
