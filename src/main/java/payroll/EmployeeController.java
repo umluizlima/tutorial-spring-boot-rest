9f3d704a2389dc68565d742e3cfd8d230d1606b9
umluizlima <umluizlima@gmail.com>
2019-03-14 11:07:36 -0300

    Add newEmployee method to EmployeeController

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -3,6 +3,8 @@ package payroll;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
     }
 
 }
